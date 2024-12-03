a466896f0ef8e46e05f75ab7fb96e4926d9b5cf0
Fabio Marques <engfabiom@gmail.com>
2024-12-03 15:40:32 -0300

    include ItemLandscape call on App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import ItemLandscape from './components/ItemLandscape'
 
 export default function App() {
-  return <div>Hello, I am the bare bones react app.</div>;
+  // return <div>Hello, I am the bare bones react app.</div>;
+  return <>
+    <ItemLandscape image='https://picsum.photos/200' alt='random image' />
+  </>
 }
