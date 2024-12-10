46a2b8a820b1fe7ea8e026fe286b871b84a14513
Fabio Marques <engfabiom@gmail.com>
2024-12-10 09:30:22 -0300

    latest changes during meeting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import ThemeToggler from './components/ThemeToggler';
-import LandscapeCard from './components/LandscapeCard';
-import { apiGetOneRandom } from './API/requests';
+import React, { useState, useEffect } from "react";
+import ThemeToggler from "./components/ThemeToggler";
+import LandscapeCard from "./components/LandscapeCard";
+import { apiGetOneRandom } from "./API/requests";
 import useLocalStorage from "use-local-storage";
 
 const App = () => {
   const themePreference = window.matchMedia("(prefers-color-scheme: dark)").matches;
   const [isDarkTheme, setIsDarkTheme] = useLocalStorage("isDarkTheme",themePreference);
-  const [theMetObject, setTheMetObject] = useState({});
+  const [theMetObjects, setTheMetObject] = useState(new Set());
+
+  const fetchTheMetRandomObject = async (n = 1) => {
+    let objects = new Set(theMetObjects);
+    let randomObj;
+    for (let i = 0; i < n; i++) {
+      randomObj = await apiGetOneRandom();
+      if (!randomObj || objects.has(randomObj))
+        return fetchTheMetRandomObject();
+      objects.add(randomObj);
+    }
+    setTheMetObject(new Set(objects));
+    console.log(theMetObjects);
+    return objects;
+  };
 
   useEffect(() => {
-    const fetchTheMetObject = async () => {
-      const randomObj = await apiGetOneRandom();
-      setTheMetObject(randomObj);
-    };
-    fetchTheMetObject();
+    fetchTheMetRandomObject(15);
   }, []);
 
   return (
-    <div id='app' className='app' data-theme={isDarkTheme ? 'dark' : 'light'}>
+    <div id="app" className="app" data-theme={isDarkTheme ? "dark" : "light"}>
       <ThemeToggler setIsDarkTheme={setIsDarkTheme} isDarkTheme={isDarkTheme} />
-      <LandscapeCard {...theMetObject} />
+      {[...theMetObjects].map((o) => (
+        <LandscapeCard key={o.objectID} {...o} />
+      ))}
     </div>
   );
 };
