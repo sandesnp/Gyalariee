f50de53676449fccbd95d5bc29c9a7762dbf6c7e
sandesnp <averagenp@gmail.com>
2025-03-04 20:46:54 +1100

    Revert "add global & index css files"
    This reverts commit 8dfc32fd2f6ff08fe70bf4d3811e290f42ec5a10.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { StrictMode } from 'react';
 import ReactDom from 'react-dom/client';
 import App from './App';
-import './css/index.css';
+
 const root = ReactDom.createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
